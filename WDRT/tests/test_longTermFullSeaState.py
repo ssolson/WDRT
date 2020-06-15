9e3d3fa1a64fc4618cc6bdd6fa0c0deee37ac270
ssolson <ssolson@sandia.gov>
2020-06-15 10:26:17 -0600

    Adjusting data path to pass test

--- WDRT/tests/test_longTermFullSeaState.py
+++ WDRT/tests/test_longTermFullSeaState.py
@@ -7,7 +7,10 @@ import os
 
 
 # Load data from example_envSampling.py
-envFile = h5py.File(os.path.join(r'../../examples/data', 'NDBC46022.h5'), 'r')
+#dataPath = os.path.join('..','..','examples','data')
+print(os.getcwd())
+dataPath = os.path.join('examples','data')
+envFile = h5py.File(os.path.join(dataPath, 'NDBC46022.h5'), 'r')
 Hs_sample = np.array(envFile['Samples_FullSeaState/Hs_SampleFSS'])
 T_sample = np.array(envFile['Samples_FullSeaState/T_SampleFSS'])
 Weight_sample = np.array(envFile['Samples_FullSeaState/Weight_SampleFSS'])
@@ -17,7 +20,7 @@ Hs_Return = np.array(envFile['ReturnContours/Hs_Return'])
 T_Return = np.array(envFile['ReturnContours/T_Return'])
 
 # Load data from modeling (if the cwd is not examples adjust the data path)
-modResFile = h5py.File(os.path.join('../../examples/data', 'longTerm_FullSeaState.h5'), 'r')
+modResFile = h5py.File(os.path.join(dataPath, 'longTerm_FullSeaState.h5'), 'r')
 t = np.array(modResFile['time'])
 tSim = t[-1]
 n = len(modResFile['x'])
@@ -43,35 +46,35 @@ for ii in range(n):
 # Long-term extreme response
 LTS = lte.fullLongTermSurvival(Fr=ccdf, fs=Weight_sample)
 
-# Plotting
-plt.figure().canvas.set_window_title('Sea state sampling')
-plt.plot(T, Hs, 'bo', alpha=0.25, label='data')
-plt.plot(T_sample, Hs_sample, 'ro', label='samples')
-plt.plot(T_Return, Hs_Return, label='100 year contour')
-plt.legend(loc='best')
-plt.grid(True)
-plt.xlabel('Energy period, $T_e$ [s]')
-plt.ylabel('Sig. wave height, $H_s$ [m]')
+## Plotting
+#plt.figure().canvas.set_window_title('Sea state sampling')
+#plt.plot(T, Hs, 'bo', alpha=0.25, label='data')
+#plt.plot(T_sample, Hs_sample, 'ro', label='samples')
+#plt.plot(T_Return, Hs_Return, label='100 year contour')
+#plt.legend(loc='best')
+#plt.grid(True)
+#plt.xlabel('Energy period, $T_e$ [s]')
+#plt.ylabel('Sig. wave height, $H_s$ [m]')
 
-plt.figure().canvas.set_window_title('Long-term response')
-plt.plot(x_t, LTS(x_t), 'k-', label='Full sea state survival')
-for ii in range(n):
-    if ii == 0:
-        plt.plot(x_t, ccdf[ii](x_t), 'r-', alpha=0.1, label='Individual sea state survivals')
-    else:
-        plt.plot(x_t, ccdf[ii](x_t), 'r-', alpha=0.1)
-plt.gca().set_yscale('log')
-plt.grid(True)
-yLs = [1, 5, 10, 25, 50, 100]
-for yL in yLs:
-    plt.plot(plt.xlim(), (1 / (yL * 365.25 * 24 * 1)) * np.ones(2), 'r--')
-    plt.text(plt.xlim()[0] + 0.05 * plt.xlim()[1],
-             (1 / (yL * 365.25 * 24 * 1)),
-             '%i-year return' % (yL), fontsize=10)
-plt.xlabel('$x$')
-plt.ylabel('$CCDF(x)$')
-plt.xlim((0, 30))
-plt.ylim((1e-1 / (yLs[-1] * 365.25 * 24 * 1), 1))
-plt.legend(loc='upper right')
+#plt.figure().canvas.set_window_title('Long-term response')
+#plt.plot(x_t, LTS(x_t), 'k-', label='Full sea state survival')
+#for ii in range(n):
+#    if ii == 0:
+#        plt.plot(x_t, ccdf[ii](x_t), 'r-', alpha=0.1, label='Individual sea state survivals')
+#    else:
+#        plt.plot(x_t, ccdf[ii](x_t), 'r-', alpha=0.1)
+#plt.gca().set_yscale('log')
+#plt.grid(True)
+#yLs = [1, 5, 10, 25, 50, 100]
+#for yL in yLs:
+#    plt.plot(plt.xlim(), (1 / (yL * 365.25 * 24 * 1)) * np.ones(2), 'r--')
+#    plt.text(plt.xlim()[0] + 0.05 * plt.xlim()[1],
+#             (1 / (yL * 365.25 * 24 * 1)),
+#             '%i-year return' % (yL), fontsize=10)
+#plt.xlabel('$x$')
+#plt.ylabel('$CCDF(x)$')
+#plt.xlim((0, 30))
+#plt.ylim((1e-1 / (yLs[-1] * 365.25 * 24 * 1), 1))
+#plt.legend(loc='upper right')
 
-plt.show()
+#plt.show()
